288729552c0076a89d5000aa62bb2f7129859ec2
Neil Bye <neil_bye@hotmail.com>
2015-02-23 20:43:04 +0000

    product.rb cache

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,9 @@ validates :image_url, allow_blank: true, format: {
 with: %r{\.(gif|jpg|png)\Z}i,
 message: 'must be a URL for GIF, JPG or PNG image.' 
 }
+def self.latest
+ 
+Product.order(:updated_at).last
+ 
+end
 end
